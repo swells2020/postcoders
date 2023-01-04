d98f33309c6536d65946f7d2d8234f850a76fd3b
swells2020 <wells.callum@outlook.com>
2023-01-04 18:21:49 +0000

    add feature 3, the ability for users to define an outcode to search.

--- src/App.jsx
+++ src/App.jsx
@@ -6,25 +6,57 @@ import './App.css'
 function App() {
 
   const [areas, setAreas] = useState([]);
+  const [newOutcode, setNewOutcode] = useState('');
+  const [submitOutcode, setSubmitOutcode] = useState('');
+  const [error, setError] = useState(false);
 
   const load = async () => {
     try {
-      const areaData = await getAreaData()
+      const areaData = await getAreaData(submitOutcode)
   
       setAreas(areaData);
     } catch (error) {
-      window.alert("todo: fix app")
+      console.log(error)
+    }
+  }
+
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+
+    const regex = new RegExp('(^[A-Z]{1,2}[0-9]{1,2}$)');
+
+    if(regex.test(newOutcode)) {
+      setError(false)
+      setSubmitOutcode(newOutcode)
+    } else {
+      setError(true)
+    }
+  
+  try {
+    await getAreaData(submitOutcode)
+  } catch (error) {
+      console.log(error)
     }
   }
 
   useEffect(() => {
     load();
-  }, []);
+  }, [submitOutcode]);
 
   return (
     <div className="App">
       <h1>Postcoders</h1>
-      <h2>{`Areas for BB10: ${areas.length}`}</h2>
+      <form onSubmit={handleSubmit}>
+        <input 
+          placeholder='Outcode to search...'
+          value={newOutcode}
+          onChange={(e) => {
+            setNewOutcode(e.target.value.toUpperCase())
+          }}        />
+        <input type="submit" value="Submit"></input>
+      </form>
+      {error ? <p>Invalid outcode format.</p> : null}
+      {areas.length ? <h2>{`Areas for ${submitOutcode}: ${areas.length}`}</h2> : null}
     </div>
   )
 }
