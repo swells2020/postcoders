3a12e369ad138b5e9a947c4cdd880a1a01932c7d
swells2020 <wells.callum@outlook.com>
2023-01-04 17:21:56 +0000

    fix bug 2, the correct number of areas now displays.

--- src/App.jsx
+++ src/App.jsx
@@ -10,10 +10,8 @@ function App() {
   const load = async () => {
     try {
       const areaData = await getAreaData()
-
-      areas.concat(areaData);
   
-      setAreas(areas);
+      setAreas(areaData);
     } catch (error) {
       window.alert("todo: fix app")
     }
